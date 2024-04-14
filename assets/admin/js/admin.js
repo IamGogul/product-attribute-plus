65f188e47c29c684ab3031455937420964bf74de
WeKinForce <wekinforce@gmail.com>
2024-04-14 08:19:52 +0530

    Untrack files in .gitignore

---
+++ assets/admin/js/admin.js
@@ -0,0 +1,10 @@
+"use strict";
+ var $ = jQuery.noConflict();
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*****************************************!*\
+  !*** ./assets/source/js/admin/index.js ***!
+  \*****************************************/
+alert("Hello World");
+/******/ })()
+;
